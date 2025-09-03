d2fbcbdffd016b428aaa61e1cee56a7803654d8d
Riley-Kay001 <riley.winchell001@albright.edu>
2025-09-03 09:34:37 -0400

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,56 @@
+# Riley Winchell
+# 2 - 1
+message = "It was a long day"
+print(message)
+# 2-2 
+message = "I had a lot to do"
+print(message)
+# 2 - 3
+first_name = "Jessica"
+last_name = "Winchell"
+Full_Name = f"{first_name} {last_name}"
+message = (f"How can I help you, {Full_Name.title()}") 
+print(message)
+# 2 - 4
+name = "Riley Winchell"
+print(name.title())
+name = "Riley Winchell"
+print(name.upper())
+print(name.lower())
+# 2 - 5
+first_name = "Martin Luther"
+last_name = "King Jr"
+full_name = f"{first_name}{last_name}"
+message = ("Martin Luther King Jr once said, \"Injustice anywhere is a threat to justice everywhere\"")
+print(message)
+# 2 - 6
+first_name = "Martin Luther"
+last_name = "King Jr"
+famous_person = f"{first_name}{last_name}"
+message = (f"{famous_person} once said, \"Injustice anywhere is a threat to justice everywhere\"")
+print(message)
+# 2 - 7
+message = "Today is the warmest day of the week"
+rstrip = ("\t" + message.rstrip())
+lstrip = ("\t" + message.lstrip())
+strip = ("\t" + message.strip())
+print(message)
+print("Whitespace Stripping:\n\t" + rstrip + lstrip + strip)
+# 2 - 8
+filename = "python_notes.txt"
+cleaned_filename = filename.removesuffix('.txt')
+print(cleaned_filename)
+# 2 - 9
+print(4+4)
+print(16/2)
+print(4*2)
+print(10-2)
+# 2 - 10
+favorite_number = "18"
+message = f"My favorite number is {favorite_number}"
+print(message)
+# 2 - 11
+# Say Martain Luther King Jr is an inspirational quote.
+print ("Martain Luther King Jr gave an inspirational quote")
+# Say Hello to Riley Winchell
+print ("Hello Riley Winchell")
