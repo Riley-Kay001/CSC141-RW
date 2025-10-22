79efcf1f08caf4bcd7d0f020f8dc1bc202d62c38
Riley-Kay001 <riley.winchell001@albright.edu>
2025-10-22 08:51:39 -0400

    Add files via upload

---
+++ Assignment 8.py
@@ -0,0 +1,42 @@
+#8 - 1 Display Message
+def display_message():
+    print("This chapter we will be learning about functions, which are named blocks of code.")
+display_message()
+#8 - 2 Favorite Book
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+favorite_book("Nancy Drew")
+favorite_book("Judy Bloom")
+favorite_book("The Outsiders")
+print(favorite_book)
+#8 - 3 T-Shirts
+def make_shirt(message = "WEST COAST", size = "small"):
+    print(f"You made a size {size} with it saying {message}")
+make_shirt("Good Morning", "medium") 
+make_shirt()
+#8 - 4 Large Shirts
+
+#8 - 5 Cities
+def describe_city(city, country = "USA"):
+    print(f"{city} is in {country}")
+describe_city("New York")
+describe_city("Los Angelas")
+describe_city ("Santorini")
+describe_city("Tokyo")
+#8 - 6 
+
+#8 - 7
+
+#8 - 8
+
+#8 - 9
+
+#8 - 10
+
+#8 - 11
+
+#8 - 12
+
+#8 - 13
+
+#8 - 14
